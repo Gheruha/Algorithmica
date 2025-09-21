67c5592650be50a95a18aada68fe7ddf86250285
Gheruha <maxgheruha@gmail.com>
2025-09-21 22:39:35 +0300

    Leetcode: problem 197 solution.

---
+++ leetcode/p197.sql
@@ -0,0 +1,8 @@
+-- Problem 197 - Rising Temperatures.
+SELECT
+  w2.id
+FROM
+  Weather w1
+  JOIN Weather w2 ON DATEDIFF (w1.recordDate, w2.recordDate) = -1
+WHERE
+  w2.temperature > w1.temperature;
