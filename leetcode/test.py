5309e7629ce12e5cd8af0c7a136595b08dad7ed5
Gheruha <maxgheruha@gmail.com>
2025-09-26 17:01:23 +0300

    Leetcode: problem 70 solution 1.

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,25 +1,3 @@
-# Palindrome/ Anagram
+x = 121
 
-# Palindrome Solution
-word = "Madam"
-if word[::-1].lower() == word.lower():
-    print("Palindrome")
-else:
-    print("Not Palindrome")
-
-""" Anagram Solution
- Given two strings s1 & s2, implement a solution to see if they are anagram or not
-
- 2 words are anagrams when:
- - they have exactly the same characters
- - they are not the same
- - the order of the characters does not matter
-"""
-
-s1 = "night"
-s2 = "thing"
-
-if len(s1) == len(s2) and sorted(s1) == sorted(s2):
-    print("Anagrams")
-else:
-    print("Not Anagrams")
+print(x // 10)
