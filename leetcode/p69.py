81214a10cb6f028ffcfdbb8685403fc7502b949d
Gheruha <maxgheruha@gmail.com>
2025-09-26 18:19:20 +0300

    Leetcode: problem 69 solution 1.

---
+++ leetcode/p69.py
@@ -0,0 +1,56 @@
+# EASY LEETCODE
+# Problem 69: Sqrt(x)
+# @leet imports start
+import bisect
+import collections
+import copy
+import datetime
+import functools
+import heapq
+import io
+import itertools
+import json
+import math
+import operator
+import random
+import re
+import statistics
+import string
+import sys
+from bisect import *
+from builtins import *
+from collections import *
+from copy import *
+from datetime import *
+from functools import *
+from heapq import *
+from io import *
+from itertools import *
+from json import *
+from math import *
+from operator import *
+from random import *
+from re import *
+from statistics import *
+from string import *
+from sys import *
+from typing import *
+
+# @leet imports end
+
+
+# @leet start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        for i in range(x + 1):
+            if i * i > x:
+                return i - 1
+
+        return -1
+
+
+# @leet end
