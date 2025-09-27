1cf163224cd0f4b1e9ea94f86b8404d11408f6b9
Gheruha <maxgheruha@gmail.com>
2025-09-27 11:39:35 +0300

    Leetcode: problem 83 solution 1.

---
+++ leetcode/p83.py
@@ -0,0 +1,67 @@
+# EASY: LEETCODE
+# Problem 83: Remove dublicates from Linked List
+# @leet imports start
+import bisect
+import collections
+import copy
+import datetime
+import functools
+import heapq
+import io
+import itertools
+import json
+import math
+import operator
+import random
+import re
+import statistics
+import string
+import sys
+from bisect import *
+from builtins import *
+from collections import *
+from copy import *
+from datetime import *
+from functools import *
+from heapq import *
+from io import *
+from itertools import *
+from json import *
+from math import *
+from operator import *
+from random import *
+from re import *
+from statistics import *
+from string import *
+from sys import *
+from typing import *
+
+# @leet imports end
+
+
+# @leet start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        nums = []
+        dummy = ListNode(0)
+        current = dummy
+
+        while head:
+            if head.val not in nums:
+                nums.append(head.val)
+                current.next = ListNode(head.val)
+                current = current.next
+            head = head.next
+
+        return dummy.next
+
+
+# @leet end
+
