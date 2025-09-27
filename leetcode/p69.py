6d85d7246a7ea9d84a10aae4ca45dc04c0a40903
Gheruha <maxgheruha@gmail.com>
2025-09-27 10:44:27 +0300

    Leetcode: problem 69 solution 2.

--- leetcode/p69.py
+++ leetcode/p69.py
@@ -42,15 +42,20 @@ from typing import *
 # @leet start
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return 0
-        if x == 1:
-            return 1
-        for i in range(x + 1):
-            if i * i > x:
-                return i - 1
+        if x < 2:
+            return x
 
-        return -1
+        l, r = 1, x // 2
+        ans = 0
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid <= x:
+                ans = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return ans
 
 
 # @leet end
