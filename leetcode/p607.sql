dfdf0c0a4eea35c1029ad9e417c6beba2d40655d
Gheruha <maxgheruha@gmail.com>
2025-09-29 19:46:26 +0300

    Leetcode: problem 607 solution.

---
+++ leetcode/p607.sql
@@ -0,0 +1,16 @@
+-- LEETCODE EASY: 607. Sales Person
+SELECT
+  s.name
+FROM
+  SalesPerson s
+WHERE
+  NOT EXISTS (
+    SELECT
+      1
+    FROM
+      Orders o
+      JOIN Company c ON o.com_id = c.com_id
+    WHERE
+      o.sales_id = s.sales_id
+      AND c.name = "RED"
+  );
