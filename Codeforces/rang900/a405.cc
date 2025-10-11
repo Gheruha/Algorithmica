4aa61faaab467c5ca1854a7646da4bffecbee293
Gheruha <maxgheruha@gmail.com>
2025-10-11 17:51:51 +0300

    Codeforces: problem 405A solution 2.

--- Codeforces/rang900/a405.cc
+++ Codeforces/rang900/a405.cc
@@ -1,9 +1,61 @@
 /* 405A - Gravity Flip. */
-// Solution 1 - using built-in algorithm functions.
+// Solution 2 - using self-made algorithms.
 #include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
+
+vector<int> mergeSort(vector<int> arr) {
+  if (arr.size() > 1) {
+    // Creating left & right array, finding the mid
+    vector<int> leftArr;
+    vector<int> rightArr;
+    int mid = arr.size() / 2;
+
+    // Populating both left & right array
+    for (int i = 0; i < mid; ++i) {
+      leftArr.push_back(arr[i]);
+    }
+    for (int i = mid; i < arr.size(); ++i) {
+      rightArr.push_back(arr[i]);
+    }
+
+    // Recursion -> splitting the array in smaller sub-arrays
+    leftArr = mergeSort(leftArr);
+    rightArr = mergeSort(rightArr);
+
+    // Comparing
+    size_t i = 0;
+    size_t j = 0;
+    size_t k = 0;
+
+    while (i < leftArr.size() && j < rightArr.size()) {
+      if (leftArr[i] < rightArr[j]) {
+        arr[k] = leftArr[i];
+        ++i;
+      } else {
+        arr[k] = rightArr[j];
+        ++j;
+      }
+
+      ++k;
+    }
+
+    // Adding leftovers
+    while (i < leftArr.size()) {
+      arr[k] = leftArr[i];
+      ++i;
+      ++k;
+    }
+
+    while (j < rightArr.size()) {
+      arr[k] = rightArr[j];
+      ++j;
+      ++k;
+    }
+  }
+  return arr;
+}
 
 int main() {
   int n;
@@ -16,7 +68,7 @@ int main() {
     cubes.push_back(cube);
   }
 
-  sort(cubes.begin(), cubes.end());
+  cubes = mergeSort(cubes);
 
   for (size_t i = 0; i < cubes.size(); ++i) {
     cout << cubes[i] << " ";
